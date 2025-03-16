feb60ed06644743524c1fb882359342c952b0939
eptle <btlk04@gmail.com>
2025-03-16 15:23:02 +0200

    косяк фикс

--- ATlearning/ATframework3demo/TestCases/Case_Bitrix24_CommentInNewsFeed.cs
+++ ATlearning/ATframework3demo/TestCases/Case_Bitrix24_CommentInNewsFeed.cs
@@ -10,60 +10,57 @@ using ATframework3demo.TestEntities;
 
 namespace ATframework3demo.TestCases
 {
-    public class Case_Bitrix24_CommentInNewsFeed
+    public class Case_Bitrix24_CommentInNewsFeed : CaseCollectionBuilder
     {
-        public class Case_Bitrix24_CRM : CaseCollectionBuilder
+        protected override List<TestCase> GetCases() // этот метод добавляет наш тест-кейс в очередь/список
         {
-            protected override List<TestCase> GetCases() // этот метод добавляет наш тест-кейс в очередь/список
-            {
-                var caseCollection = new List<TestCase>();
-                caseCollection.Add(new TestCase("Создание комментария от другого юзера под постом в ленте", homePage => SeeCommentByOtherUser(homePage)));
-                return caseCollection;
-            }
-
-            void SeeCommentByOtherUser(PortalHomePage homePage)
-            {
-                var user1 = new Bitrix24CRMcontacts { Name = "user1_" + HelperMethods.GetDateTimeSaltString() };
-                user1.CreateByAPI(TestCase.RunningTestCase.TestPortal);
-                // выберем первый попавшийся пост из ленты и возьмем его айди
-                string postID = new WebItem("(//div[starts-with(@id, 'blg-post-')])[1]", "первый пост в ленте").GetAttribute("id");
-                string text = "super_duper_text_" + HelperMethods.GetDateTimeSaltString();
-
-                // создаем объект поста
-                var post = homePage
-                // переходим в новостную ленту
+            var caseCollection = new List<TestCase>();
+            caseCollection.Add(new TestCase("Создание комментария от другого юзера под постом в ленте", homePage => SeeCommentByOtherUser(homePage)));
+            return caseCollection;
+        }
+        
+        void SeeCommentByOtherUser(PortalHomePage homePage)
+        {
+            var user1 = new Bitrix24CRMcontacts { Name = "user1_" + HelperMethods.GetDateTimeSaltString() };
+            user1.CreateByAPI(TestCase.RunningTestCase.TestPortal);
+            // выберем первый попавшийся пост из ленты и возьмем его айди
+            string postID = new WebItem("(//div[starts-with(@id, 'blg-post-')])[1]", "первый пост в ленте").GetAttribute("id");
+            string text = "super_duper_text_" + HelperMethods.GetDateTimeSaltString();
+        
+            // создаем объект поста
+            var post = homePage
+            // переходим в новостную ленту
+            .NewsFeed
+            // выбираем по айди пост в ленте
+            .OpenPost(postID);
+        
+            post.ID = postID;
+        
+            // тыкаем по полю ввода комментария
+            var comment = post.ClickOnCommentArea();
+            // добавить текст в комментарий
+            comment.AddTextToComment(text);
+            // отправить комментарий под постом и получить айди комментария
+            string commentID = comment.Send().ID;
+        
+            // ---------------------------------------
+        
+            var user2 = TestCase.RunningTestCase.CreatePortalTestUser(false);
+        
+            WebItem.DefaultDriver.Quit();
+            WebItem.DefaultDriver = default;
+        
+            // проверка с другого юзера, что текст комментария не отличается
+            string commentText = new PortalLoginPage(TestCase.RunningTestCase.TestPortal)
+                .Login(user2)
                 .NewsFeed
                 // выбираем по айди пост в ленте
-                .OpenPost(postID);
-
-                post.ID = postID;
-
-                // тыкаем по полю ввода комментария
-                var comment = post.ClickOnCommentArea();
-                // добавить текст в комментарий
-                comment.AddTextToComment(text);
-                // отправить комментарий под постом и получить айди комментария
-                string commentID = comment.Send().ID;
-
-                // ---------------------------------------
-
-                var user2 = TestCase.RunningTestCase.CreatePortalTestUser(false);
-
-                WebItem.DefaultDriver.Quit();
-                WebItem.DefaultDriver = default;
-
-                // проверка с другого юзера, что текст комментария не отличается
-                string commentText = new PortalLoginPage(TestCase.RunningTestCase.TestPortal)
-                    .Login(user2)
-                    .NewsFeed
-                    // выбираем по айди пост в ленте
-                    .OpenPost(postID) 
-                    .GetCommentTextByID(commentID);
-
-                if (commentText != text )
-                {
-                    Log.Error("Тексты не совпадают");
-                }
+                .OpenPost(postID) 
+                .GetCommentTextByID(commentID);
+        
+            if (commentText != text )
+            {
+                Log.Error("Тексты не совпадают");
             }
         }
     }
