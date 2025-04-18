fe8bbf2e9fcee632dddbdba53f48718651986d31
eptle <119448545+eptle@users.noreply.github.com>
2025-04-19 00:30:56 +0200

    тест-кейсы для проверки работы тулбара

---
+++ ATlearning/ATframework3demo/TestCases/Skillmap/Case_Bitrix24_Toolbar.cs
@@ -0,0 +1,62 @@
+﻿using AquaTestFramework.CommonFramework.BitrixCPinteraction;
+using atFrameWork2.BaseFramework;
+using atFrameWork2.BaseFramework.LogTools;
+using atFrameWork2.PageObjects;
+using atFrameWork2.SeleniumFramework;
+using ATframework3demo.BaseFramework;
+using ATframework3demo.PageObjects.CRM;
+using ATframework3demo.PageObjects.NewsFeed;
+using ATframework3demo.TestEntities;
+
+namespace ATframework3demo.TestCases
+{
+    public class Case_Bitrix24_Toolbar : CaseCollectionBuilder
+    {
+        protected override List<TestCase> GetCases()
+        {
+            var caseCollection = new List<TestCase>();
+            caseCollection.Add(new TestCase("Открыть 'создание профиля'", homePage => openProfilesPage(homePage)));
+            caseCollection.Add(new TestCase("Открыть 'skillmap'", homePage => openSkillmapMainPage(homePage)));
+            caseCollection.Add(new TestCase("Открыть 'мой прогресс'", homePage => openMyProgressPage(homePage)));
+            caseCollection.Add(new TestCase("Открыть 'все аттестации'", homePage => openAllAttestationPage(homePage)));
+            return caseCollection;
+        }
+
+        void openProfilesPage(PortalHomePage homePage)
+        {
+
+            homePage
+                .LeftMenu
+                .OpenSkillMap()
+                .Toolbar                    // объект тулбара
+                .ClickOnAddProfileBtn();    // кликнуть на кнопку добавить 
+        }
+
+        void openSkillmapMainPage(PortalHomePage homePage)
+        {
+            homePage
+                .LeftMenu
+                .OpenSkillMap()
+                .Toolbar                            // объект тулбара
+                .ClickOnSpecialistProfilesBtn();    // кликнуть на кнопку профили специалистов
+        }
+
+        void openMyProgressPage(PortalHomePage homePage)
+        {
+            homePage
+                .LeftMenu
+                .OpenSkillMap()
+                .Toolbar                    // объект тулбара
+                .ClickOnMyProgressBtn();    // кликнуть на кнопку мой прогресс
+        }
+
+        void openAllAttestationPage(PortalHomePage homePage)
+        {
+            homePage
+                .LeftMenu
+                .OpenSkillMap()
+                .Toolbar                    // объект тулбара
+                .ClickOnAnalyticsBtn(1);    // кликнуть на кнопку аналитика и отчеты -> все аттестации
+        }
+    }
+}
