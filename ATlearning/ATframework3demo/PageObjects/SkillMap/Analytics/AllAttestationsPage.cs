7a6d111c4efd39bc9b807ff546d3d0e2f07793f2
Adelya Zamuldinova <adelia.231.nat@gmail.com>
2025-04-20 19:10:28 +0200

    Update AllAttestationsPage.cs
    добавила  TopToolbar

--- ATlearning/ATframework3demo/PageObjects/SkillMap/Analytics/AllAttestationsPage.cs
+++ ATlearning/ATframework3demo/PageObjects/SkillMap/Analytics/AllAttestationsPage.cs
@@ -1,14 +1,18 @@
-﻿using OpenQA.Selenium;
+﻿using atFrameWork2.SeleniumFramework;
+using ATframework3demo.PageObjects.SkillMap.Components;
+using OpenQA.Selenium;
 
 namespace ATframework3demo.PageObjects.SkillMap.Analytics
 {
     public class AllAttestationsPage
     {
         public IWebDriver Driver { get; }
+        public TopToolbar Toolbar { get; }
 
         public AllAttestationsPage(IWebDriver driver = default)
         {
             Driver = driver;
+            Toolbar = new TopToolbar(driver);
         }
     }
 }
