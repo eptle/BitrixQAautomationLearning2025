1eb5bd805cbeb456d14896e7e7b8643f16cae306
Adelya Zamuldinova <adelia.231.nat@gmail.com>
2025-04-20 20:38:11 +0200

    Update CertificationRelevance.cs

--- ATlearning/ATframework3demo/PageObjects/SkillMap/Analytics/CertificationRelevance.cs
+++ ATlearning/ATframework3demo/PageObjects/SkillMap/Analytics/CertificationRelevance.cs
@@ -1,14 +1,28 @@
-﻿using OpenQA.Selenium;
+﻿using atFrameWork2.SeleniumFramework;
+using ATframework3demo.PageObjects.SkillMap.Components;
+using OpenQA.Selenium;
 
 namespace ATframework3demo.PageObjects.SkillMap.Analytics
 {
     public class CertificationRelevance
     {
         public IWebDriver Driver { get; }
+        public TopToolbar Toolbar { get; }
 
         public CertificationRelevance(IWebDriver driver = default)
         {
             Driver = driver;
+            Toolbar = new TopToolbar(driver);
         }
-    }
+
+        public object ActionToEmployee(string employeeName, int listItemNumber)
+        {
+            var burger = new WebItem(
+                $"//span[contains(text(), '{employeeName}')]/../../..//a",
+                $"Бургер напротив сотрудника с именем {employeeName}");
+
+            burger.Click();
+            throw new NoSuchElementException();
+            }
+        }
 }
