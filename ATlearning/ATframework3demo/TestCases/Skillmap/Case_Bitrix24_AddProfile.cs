6e910a7c2171a6e7e324ece6c2bfa7802843dac5
Dmitry But <btlk04@gmail.com>
2025-04-20 19:16:19 +0200

    добавил тест-кейсы

---
+++ ATlearning/ATframework3demo/TestCases/Skillmap/Case_Bitrix24_AddProfile.cs
@@ -0,0 +1,48 @@
+﻿using atFrameWork2.BaseFramework;
+using atFrameWork2.PageObjects;
+using ATframework3demo.BaseFramework;
+using ATframework3demo.BaseFramework.BitrixCPinterraction;
+using ATframework3demo.PageObjects.SkillMap;
+
+namespace ATframework3demo.TestCases.Skillmap
+{
+    public class Case_Bitrix24_AddProfile : CaseCollectionBuilder
+    {
+        protected override List<TestCase> GetCases()
+        {
+            var caseCollection = new List<TestCase>();
+            caseCollection.Add(new TestCase("Добавить профиль с тремя скиллами и нажать создать", homePage => createProfile(homePage)));
+            caseCollection.Add(new TestCase("Добавить профиль с одним скиллом и нажать отмена", homePage => cancelCreatingProfile(homePage)));
+            return caseCollection;
+        }
+        void createProfile(PortalHomePage homePage)
+        {
+            string date = HelperMethods.GetDateTimeSaltString();
+            string profileName = "profile_1_" + date;
+            string skill1 = "Skill_1_ " + date;
+            string skill2 = "Skill_2_ " + date;
+            string skill3 = "Skill_3_ " + date;
+            int[] grades = { 10, 20, 30 };
+
+            var ProfilePage = homePage
+                .GoToSkillmap()
+                .ClickOnAddProfileBtn()
+                .ClickOnAddSkillBtn()
+                .ClickOnAddSkillBtn()
+                .FillSkillForm(1, skill1, grades)
+                .FillSkillForm(2, skill2, grades)
+                .FillSkillForm(3, skill3, grades)
+                .ClickOnRemoveSkillBtn(2)
+                .InputProfileName(profileName)
+                .ClickOnCreateProfileBtn()
+                .ChooseActionToProfile(profileName, 1);
+            Waiters.StaticWait_s(5);
+        }
+
+        void cancelCreatingProfile(PortalHomePage homePage)
+        {
+
+        }
+
+    }
+}
---
+++ ATlearning/ATframework3demo/TestCases/Skillmap/Case_Bitrix24_SkillmapMainPage.cs
@@ -0,0 +1,51 @@
+﻿using atFrameWork2.BaseFramework;
+using atFrameWork2.PageObjects;
+using ATframework3demo.BaseFramework.BitrixCPinterraction;
+using ATframework3demo.PageObjects.SkillMap;
+
+namespace ATframework3demo.TestCases.Skillmap
+{
+    public class Case_Bitrix24_SkillmapMainPage : CaseCollectionBuilder
+    {
+        protected override List<TestCase> GetCases()
+        {
+            var caseCollection = new List<TestCase>();
+            caseCollection.Add(new TestCase("Создать 25 специалистов", homePage => query(homePage)));
+            caseCollection.Add(new TestCase("Прокликать гриды (след, пред, выкл)", homePage => testGrid(homePage)));
+            caseCollection.Add(new TestCase("Открыть 'аттестовать сотрудника'", homePage => testBurger(homePage)));
+            return caseCollection;
+        }
+        void query(PortalHomePage homePage)
+        {
+            var testPortal = TestCase.RunningTestCase.TestPortal;
+            string query = File.ReadAllText(@"../../../TestEntities/queries/clear_db_and_create_25_specialists.sql");
+            TestCase.RunningTestCase.ExecutePHP("global $DB;\r\n" +
+                $"$res = $DB->Query(\"{query}\");" +
+            @"$rows = [];
+            while ($row = $res->Fetch()) 
+	            $rows[] = $row;
+            echo json_encode($rows);");
+
+            homePage
+                .GoToSkillmap();
+        }
+
+        void testGrid(PortalHomePage homePage)
+        {
+            SkillmapMainPage SkillmapMain = homePage
+                .GoToSkillmap();                     // перейти во вкладу skillmap по uri
+
+            SkillmapMain.NextPage();
+            SkillmapMain.PreviousPage();
+            SkillmapMain.TogglePagination();
+            Waiters.StaticWait_s(5);
+        }
+
+        void testBurger(PortalHomePage homePage)
+        {
+            homePage
+                .GoToSkillmap()                                 // перейти во вкладу skillmap по uri
+                .ChooseActionToProfile("Project Manager", 3);   // тыкнуть на бургер напротив Project Manager и выбрать "Аттестовать сотрудника"
+        }
+    }
+}
