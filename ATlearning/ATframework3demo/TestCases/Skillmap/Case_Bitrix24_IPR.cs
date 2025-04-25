dbb27aa8aad1f574d7e907ae246978e09766b47b
Adelya Zamuldinova <adelia.231.nat@gmail.com>
2025-04-25 11:22:45 +0200

    Create Case_Bitrix24_IPR.cs
    добавила класс с кейсами взаимодействие с ИПР

---
+++ ATlearning/ATframework3demo/TestCases/Skillmap/Case_Bitrix24_IPR.cs
@@ -0,0 +1,34 @@
+﻿using atFrameWork2.BaseFramework;
+using atFrameWork2.PageObjects;
+
+namespace ATframework3demo.TestCases.Skillmap
+{
+    public class Case_Bitrix24_IPR : CaseCollectionBuilder
+    {
+        protected override List<TestCase> GetCases()
+        {
+            var caseCollection = new List<TestCase>();
+            caseCollection.Add(new TestCase("Формирование ИПР для сотрудника", homePage => CreateIPR(homePage)));
+            caseCollection.Add(new TestCase("Проверка отображения созданного ИПР в задачах", homePage => TaskIPR(homePage)));
+            caseCollection.Add(new TestCase("Взаимодействие с ИПР через задачу и проверка статуса его выполнения", homePage => StatusIPR(homePage)));
+            caseCollection.Add(new TestCase("Просмотр списка ИПР", homePage => ListIPRview(homePage)));
+            return caseCollection;
+        }
+        void CreateIPR(PortalHomePage homePage)
+        {
+
+        }
+        void TaskIPR(PortalHomePage homePage)
+        {
+
+        }
+        void StatusIPR(PortalHomePage homePage) 
+        {
+
+        }
+        void ListIPRview(PortalHomePage homePage)
+        {
+
+        }
+    }
+}
