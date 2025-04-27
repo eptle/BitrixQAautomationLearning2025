d225e14a4ce9442c3692fb5013ecdcda8bc69ea6
Adelya Zamuldinova <adelia.231.nat@gmail.com>
2025-04-27 19:50:04 +0200

    Update IPRstatPage.cs

--- ATlearning/ATframework3demo/PageObjects/SkillMap/IPR/IPRstatPage.cs
+++ ATlearning/ATframework3demo/PageObjects/SkillMap/IPR/IPRstatPage.cs
@@ -1,6 +1,34 @@
-﻿namespace ATframework3demo.PageObjects.SkillMap.IPR
+﻿using atFrameWork2.SeleniumFramework;
+using ATframework3demo.PageObjects.Company;
+using ATframework3demo.PageObjects.SkillMap.Components;
+using OpenQA.Selenium;
+
+namespace ATframework3demo.PageObjects.SkillMap.IPR
 {
     public class IPRstatPage
     {
+        public TopToolbar Toolbar { get; }
+
+        public IWebDriver Driver { get; }
+
+        public IPRstatPage(IWebDriver driver = default)
+        {
+            Driver = driver;
+            Toolbar = new TopToolbar(driver);
+        }
+
+        public WebItem ChooseEmployeeBtn { get; } = new WebItem(
+            "//select[@name='employee_id']",
+            "Выпадающий список выбора сотруднкиа для создания ИПР");
+        /*public WebItem EmployeeBtn { get; } = new WebItem(
+            "//option[contains(text(), {employeeName})]",
+            "Выпадающий список выбора сотруднкиа для создания ИПР");*/
+
+        public IPRlistPage CreateIPR(string employeeName)
+        {
+            ChooseEmployeeBtn.Click();
+            //EmployeeBtn.Click();
+            return new IPRlistPage();
+        }
     }
 }
