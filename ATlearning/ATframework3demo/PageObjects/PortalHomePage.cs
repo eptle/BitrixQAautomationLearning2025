b904b3bc27ce74714ef7d6134241fd42cb3b9016
Adelya Zamuldinova <adelia.231.nat@gmail.com>
2025-04-27 16:08:19 +0200

    Добавила переход на страницу сотрудников компании

--- ATlearning/ATframework3demo/PageObjects/PortalHomePage.cs
+++ ATlearning/ATframework3demo/PageObjects/PortalHomePage.cs
@@ -1,5 +1,6 @@
 ﻿using atFrameWork2.SeleniumFramework;
 using ATframework3demo.PageObjects;
+using ATframework3demo.PageObjects.Company;
 using ATframework3demo.PageObjects.NewsFeed;
 using ATframework3demo.PageObjects.SkillMap;
 using OpenQA.Selenium;
@@ -32,6 +33,12 @@ namespace atFrameWork2.PageObjects
             string SkillmapUri = "http://bitrix.dev.bx/skillmap/";
             WebDriverActions.OpenUri(new Uri(SkillmapUri));
             return new SkillmapMainPage();
+        }
+        public EmployeeListPage GoToEmployee()
+        {
+            string EmployeeListUri = "http://bitrix.dev.bx/company/";
+            WebDriverActions.OpenUri(new Uri(EmployeeListUri));
+            return new EmployeeListPage();
         }
     }
 }
