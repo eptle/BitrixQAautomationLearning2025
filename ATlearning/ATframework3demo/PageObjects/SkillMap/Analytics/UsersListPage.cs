902e0e02eda5f17d39f4299cacef39c6944be769
Dmitry But <btlk04@gmail.com>
2025-04-30 14:14:08 +0200

    Create UsersListPage.cs

---
+++ ATlearning/ATframework3demo/PageObjects/SkillMap/Analytics/UsersListPage.cs
@@ -0,0 +1,18 @@
+﻿using ATframework3demo.PageObjects.SkillMap.Components;
+using OpenQA.Selenium;
+
+namespace ATframework3demo.PageObjects.SkillMap.Analytics
+{
+    public class UsersListPage : BaseGridPage
+    {
+        public TopToolbar Toolbar { get; }
+
+        public IWebDriver Driver { get; }
+
+        public UsersListPage(IWebDriver driver = default)
+        {
+            Driver = driver;
+            Toolbar = new TopToolbar(driver);
+        }
+    }
+}
