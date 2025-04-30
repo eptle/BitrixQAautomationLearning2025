04695a335a6b2bdd160be0bceec6c69061749b07
Dmitry But <btlk04@gmail.com>
2025-04-30 14:16:50 +0200

    добавил попап на страницу статистики по профилям

---
+++ ATlearning/ATframework3demo/PageObjects/SkillMap/Components/PopUps/StatByProfilesPopup.cs
@@ -0,0 +1,48 @@
+﻿using atFrameWork2.SeleniumFramework;
+using OpenQA.Selenium;
+
+namespace ATframework3demo.PageObjects.SkillMap.Components.PopUps
+{
+    public class StatByProfilesPopup
+    {
+        private string ID;
+
+        public IWebDriver Driver { get; }
+
+        public StatByProfilesPopup(string profileName, IWebDriver driver = default)
+        {
+            Driver = driver;
+            var burger = new WebItem(
+                $"//span[contains(text(), '{profileName}')]/../../..//a",
+                $"Бургер напротив элемента с именем {profileName}");
+
+            burger.Click();
+
+            var getID = new WebItem(
+                $"//span[contains(text(), '{profileName}')]/../../../td[2]//span",
+                $"ID профиля {profileName}");
+
+            ID = getID.InnerText();
+        }
+
+        public CertificationListPage SeeCertifications()
+        {
+            var SeeCertificationsBtn = new WebItem(
+            $"(//div[@class='menu-popup-items']/a[contains(@href, '/{ID}/')])[4]",
+            "Кнопка 'посмотреть аттестации'");
+            SeeCertificationsBtn.Click();
+
+            return new CertificationListPage();
+        }
+
+        public CircularDiagramPage SeeStatistics()
+        {
+            var SeeCertificationsBtn = new WebItem(
+            $"(//div[@class='menu-popup-items']/a[contains(@href, '/{ID}/')])[5]",
+            "Кнопка 'посмотреть статистику'");
+            SeeCertificationsBtn.Click();
+
+            return new CircularDiagramPage();
+        }
+    }
+}
