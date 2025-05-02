ca5e2455b835eab622c4ec367b6c42c65725118a
Dmitry But <btlk04@gmail.com>
2025-05-02 22:55:49 +0200

    Дописал автотест Аттестовать юзера трижды (джун, сеньор, миддл) по 1 профилю и посмотреть прогресс сотрудника"

--- ATlearning/ATframework3demo/TestCases/Skillmap/SmokeTest/Class_Bitrix24_GradeUser.cs
+++ ATlearning/ATframework3demo/TestCases/Skillmap/SmokeTest/Class_Bitrix24_GradeUser.cs
@@ -6,6 +6,8 @@ using atFrameWork2.SeleniumFramework;
 using ATframework3demo.BaseFramework;
 using ATframework3demo.PageObjects.CRM;
 using ATframework3demo.PageObjects.NewsFeed;
+using ATframework3demo.PageObjects.SkillMap;
+using ATframework3demo.PageObjects.SkillMap.Analytics;
 using ATframework3demo.TestEntities;
 
 namespace ATframework3demo.TestCases.Skillmap.SmokeTest
@@ -23,6 +25,9 @@ namespace ATframework3demo.TestCases.Skillmap.SmokeTest
             caseCollection.Add(
                 new TestCase("Просмотр таблицы всех аттестаций сотрудников",
                 homePage => Create2ProfilesAndCertificate2UsersTwice(homePage)));
+            caseCollection.Add(
+                new TestCase("Аттестовать юзера трижды (джун, сеньор, миддл) по 1 профилю и посмотреть прогресс сотрудника",
+                homePage => GradeUserThrice(homePage)));
             return caseCollection;
         }
 
@@ -88,10 +93,11 @@ namespace ATframework3demo.TestCases.Skillmap.SmokeTest
             int[] grades = { 10, 20, 30 };
             string username1 = "test1";
             string username2 = "test2";
+            string admin = TestCase.RunningTestCase.TestPortal.PortalAdmin.NameLastName;
 
             var ProfilePage = homePage
                 .GoToSkillmap();
-            
+
             // создаем два профиля специалиста
             foreach (string profile in new string[] {profileName1, profileName2})
             {
@@ -101,7 +107,8 @@ namespace ATframework3demo.TestCases.Skillmap.SmokeTest
                     .InputProfileName(profile)
                     .ClickOnCreateProfileBtn();
             }
-
+            ProfilePage.SortBy("UPDATED_AT");
+            
             // аттестовать каждого из двух сотрудников по каждому профилю
             foreach (string profile in new string[] { profileName1, profileName2 })
             {
@@ -110,7 +117,6 @@ namespace ATframework3demo.TestCases.Skillmap.SmokeTest
                     ProfilePage
                         .ClickOnBurger(profile)
                         .CertificateEmployee()
-                        .SelectProfile(profile)
                         .SelectUser(username)
                         .GradeUser(new int[] { 20 })
                         .ClickOnSertificateBtn()
@@ -118,20 +124,72 @@ namespace ATframework3demo.TestCases.Skillmap.SmokeTest
                         .ClickOnSpecialistProfilesBtn();
                 }
             }
-            
+
             var AllCertification = ProfilePage
                  .Toolbar
                  .ClickOnAnalyticsBtn()
-                 .AllAttestations()
-                 .IsDataCorrect(new List<string> { 
-                     "1", 
-                     "30 апреля, 15:27", 
-                     "test1 test1", 
-                     "Дмитрий Бут", 
-                     "profile_1_30042025152651", 
-                     "Skill_1_ 30042025152651: 20 (Middle)", 
-                     "Middle (100%)" });
+                 .AllAttestations();
 
+            List<string> record = new List<string> {
+                     "",
+                     "",
+                     $"{username1}",
+                     $"Дмитрий",
+                     $"{profileName1}",
+                     "20 (Middle)",
+                     "Middle (100%)" };
+
+            bool isRecordExist = AllCertification.IsDataCorrect(record);
+
+            if (!isRecordExist)
+                Log.Error($"Записи {String.Join(", ", record)} нет в таблице");
+
+        }
+
+        void GradeUserThrice(PortalHomePage homePage)
+        {
+            string date = HelperMethods.GetDateTimeSaltString();
+            string profileName = "profile_1_" + date;
+            string skill1 = "Skill_1_ " + date;
+            int[] grades = { 10, 20, 30 };
+            string username = "test1";
+
+            var ProfilePage = homePage
+                .GoToSkillmap()
+                .ClickOnAddProfileBtn()
+                .InputProfileName(profileName)
+                .FillSkillForm(1, skill1, grades)
+                .ClickOnCreateProfileBtn();
+
+            int[] gradesForAttestation = new int[] { 12, 30, 24 };
+
+            foreach (int grade in gradesForAttestation)
+            {
+                ProfilePage
+                    .ClickOnBurger(profileName)
+                    .CertificateEmployee()
+                    .SelectUser(username)
+                    .GradeUser(new int[] { grade })
+                    .ClickOnSertificateBtn()
+                    .Toolbar
+                    .ClickOnSpecialistProfilesBtn();
+            }
+
+            var ProgressPage = ProfilePage
+                .Toolbar
+                .ClickOnAnalyticsBtn()
+                .UsersList()
+                .ClickOnBurger(username)
+                .UserProgress();
+
+            bool isProgressCorrect = ProgressPage.CheckProgress(profileName, grade: "Middle", skills: new List<string> { skill1 }, scores: new List<int> { 24 });
+
+            if (!isProgressCorrect)
+            {
+                Log.Error($"На странице Мой прогресс пользователя '{username}' неверные данные");
+            }
+
+            Waiters.StaticWait_s(3);
         }
     }
 }
