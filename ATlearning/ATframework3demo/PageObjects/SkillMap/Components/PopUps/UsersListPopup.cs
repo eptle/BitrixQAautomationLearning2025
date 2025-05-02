9ef266a8a248fd2d741307473aedf0d73cbbac63
Dmitry But <btlk04@gmail.com>
2025-05-02 22:52:51 +0200

    попап на странице Список пользователей

---
+++ ATlearning/ATframework3demo/PageObjects/SkillMap/Components/PopUps/UsersListPopup.cs
@@ -0,0 +1,30 @@
+﻿using atFrameWork2.SeleniumFramework;
+using OpenQA.Selenium;
+
+namespace ATframework3demo.PageObjects.SkillMap.Components.PopUps
+{
+    public class UsersListPopup
+    {
+        public IWebDriver Driver { get; }
+
+        public UsersListPopup(string username, IWebDriver driver = default)
+        {
+            Driver = driver;
+            var burger = new WebItem(
+                $"//span[contains(text(), '{username}')]/../../..//a",
+                $"Бургер напротив элемента с именем {username}");
+
+            burger.Click();
+        }
+
+        public MyProgressPage UserProgress()
+        {
+            var CertificateEmployeeBtn = new WebItem(
+            $"//div[@class='menu-popup-items']//a[contains(@href, '/progress/')]",
+            "Кнопка 'аттестовать сотрудника'");
+            CertificateEmployeeBtn.Click();
+
+            return new MyProgressPage();
+        }
+    }
+}
